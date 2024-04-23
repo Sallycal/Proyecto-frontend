a05b9808b76f767d16c7742383ed95f5c2831d48
Tu Nombre <sallycal2005@gmail.com>
2024-04-23 09:24:41 -0500

    toppings

---
+++ toppings-de-pizza/src/js/app.js
@@ -0,0 +1,28 @@
+// const contenedor = document.getElementById('contenedor');
+// const seleccionarElementoNaranja = document.getElementsByClassName('color-naranja')
+// const title = document.getElementsByTagName('h1');
+
+// const itens = document.querySelectorAll('.topping'); 
+// itens[0].addEventlistener(click, function(){
+//     alert("Mozzarella")
+// });
+
+// Agregar estilo con javascript
+// const primerTopping = document.querySelector('.topping');
+// primerTopping.style.cursor = "pointer";
+// primerTopping.style.backgroupColor = "blue";
+// primerTopping.style.textTransform = "uppercase";
+// console.log(primerTopping.style);
+
+// MANIFESTACION TEXTO DE DOM
+// const listaToppings = document.getElementById('lista-topping');
+// const title = document.getElementById('titleTopping');
+
+console.log("innerText->");
+console.log("");
+
+
+
+
+
+
