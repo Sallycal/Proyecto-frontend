a9c745782c88672812f0212f444008e96214f199
Tu Nombre <sallycal2005@gmail.com>
2024-05-02 23:24:04 -0500

    integracion api

--- 02-clase-weather-app/src/counter.js
+++ 02-clase-weather-app/src/counter.js
@@ -1,11 +1,22 @@
 
-//import { data } from "autoprefixer"
+// //import { data } from "autoprefixer"
 
-export async function getDataWeather (nameCity = "sincelejo") {
-  const data = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${nameCity}&appid=35afc3bb379789d81ab729809a61014c&lang=es`)
-    .then(Response => Response.json())
-   .then(resp => resp)
-   .catc(error => error.message)
+export async function getDataWeather (nameCity = 'Sincelejo') {
+  try {
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${nameCity}&appid=35afc3bb379789d81ab729809a61014c&lang=es`)
+    const data = await response.json();
 
-  return data;
+    if (!response.ok) {
+      throw new Error(data.message ||'Error en la solicitud del API');
+    }
+
+    if (data.cod !== 200) {
+      throw new Error(data.message || "Resultados no encontrados");
+    }
+
+    return data;
+
+  } catch (error) {
+    alert(error.message); 
+  }
 }
--- 02-clase-weather-app/src/main.js
+++ 02-clase-weather-app/src/main.js
@@ -1,7 +1,17 @@
 import { getDataWeather } from './counter'
 import './index.css'
 
-const data = await getDataWeathe('') //nombre de la ciudad
-
+const data = await  getDataWeather() 
+const temp = document.getElementsByClassName('temp');
+const city = document.getElementsByClassName('name-city');
+const humidity = document.getElementsByClassName('humidity');
+const wind = document.getElementsByClassName('wind');
 
 console.log(data);
+// console.log(data.main.temp) 
+// console.log('temp', temp);
+
+temp[0].innerHTML = Math.round(data.main.temp) + " ºc";
+city[0].innerHTML = data.name;
+humidity[0].innerHTML = data.main.humidity + "%";
+wind[0].innerHTML = data.wind.speed + " km/h";
